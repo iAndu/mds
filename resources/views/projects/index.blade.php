9dc75c20baeee4d849b986ba55741d5ae951bccd
Andrei <andrei.zugravu97@gmail.com>
2018-05-06 14:50:28 +0300

    refactored projects aspect

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,18 +6,30 @@
 <div class="row">
     @foreach ($collection as $project)
     <div class="col-md-4">
-        <div class="panel panel-white">
+        <div class="thumbnail">
             <div class="panel-heading">
-                <h6 class="panel-title"> {{ $project->name }} <a class="heading-elements-toggle"><i class="icon-more"></i></a></h6>
+                <h6 class="panel-title">{{ $project->name }}<a class="heading-elements-toggle"><i class="icon-more"></i></a></h6>
                 <div class="heading-elements">
                     <ul class="icons-list">
-                        <li><a href="#" class="animation" data-animation="rollIn"><i class="icon-play3"></i></a></li>
+                        <li><a href="#"><i class="icon-google-drive"></i></a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
                     </ul>
                 </div>
             </div>
 
-            <div class="panel-body">
-                <img src="storage/{{ $project->avatar }}">
+            <div class="thumb">
+                <img src="{{ URL::asset('limitless/assets/images/placeholder.jpg') }}" alt="">
+                <div class="caption-overflow">
+									<span>
+                                        <a href="#" class="btn bg-success-400 btn-icon legitRipple"><i class="icon-eye"></i></a>
+										<a href="#" class="btn bg-success-400 btn-icon legitRipple"><i class="icon-pencil"></i></a>
+									</span>
+                </div>
+            </div>
+
+            <div class="caption">
+
             </div>
         </div>
     </div>
@@ -26,3 +38,12 @@
 @endforeach
 
 @endsection
+
+@push('js')
+
+    <script type="text/javascript" src="{{ URL::asset('limitless/assets/js/plugins/forms/styling/switchery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('limitless/assets/js/plugins/media/fancybox.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('limitless/assets/js/pages/components_thumbnails.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('limitless/assets/js/pages/animations_css3.js') }}"></script>
+
+@endpush
